b6196d1185ee0f9f305332fec2c2f3122f4fb86a
arcearta <sura2016>
2016-08-24 10:54:03 -0500

    Cambios en la aplicacion para usar play

---
+++ app/controllers/Application.scala
@@ -0,0 +1,155 @@
+package controllers
+
+import java.util.Calendar
+
+import dto._
+import play.api._
+import play.api.mvc._
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.cache.Cache
+import play.api.Play.current
+import play.api.data.Form
+import play.api.libs.json.Format
+import play.api.libs.json.JsValue
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.db._
+import pokemon.PokemonServices
+import play.api.libs.json._
+
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.libs.concurrent.Execution.Implicits._
+
+import scala.concurrent.ExecutionContext.Implicits.global._
+
+trait Protocols {
+  implicit val pokemonPositionFormat = Json.writes[Position]
+  implicit val pokemonPositionReadFormat = Json.reads[Position]
+
+  implicit val pokemonInfoFormat = Json.writes[PokemonPosition]
+
+  implicit val findPokemonFormat = Json.writes[FindPokemon]
+  implicit val findPokemonReadFormat = Json.reads[FindPokemon]
+
+
+
+  implicit val findGymFormat = Json.writes[Gym]
+  implicit val findStopFormat = Json.writes[Stop]
+
+
+}
+
+
+trait Service extends Protocols {
+
+}
+
+object Application  extends Controller with Service {
+
+  val pokemonList = List(
+    PokemonPosition(1, "Pikachu", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(2, "Magikarp", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(3, "Rattata", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(4, "Hitmonchan", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(5, "Snorlax", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(6, "Jynx", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(7, "Grimer", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(8, "Koffing", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(9, "Drowzee", 0, Some(Position(-123.0881,38.3845))),
+    PokemonPosition(10, "Drowzee", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(11, "Ditto", 0, Some(Position(-122.0881,37.3845))),
+    PokemonPosition(12, "Squirtle", 0, Some(Position(-122.0881,37.3845)))
+  )
+
+  val gymList = List(
+    Gym("RED", Position(6.25376517, -75.56812763)),
+    Gym("BLUE", Position(6.254115,-75.578935))
+  )
+
+  val stopList = List(
+    Stop("", Position(6.25395714, -75.56051016)),
+    Stop("", Position(6.254085,-75.579945))
+  )
+
+
+
+  val activePokList = List(
+    PokemonPosition(1, "Pikachu", Calendar.getInstance().getTime().getTime + 1000 , Some(Position(6.25024572 , -75.56576729))),
+    PokemonPosition(5, "Magikarp", Calendar.getInstance().getTime().getTime + 2000, Some(Position(6.24815536, -75.5616389))),
+    PokemonPosition(9, "Magikarp", Calendar.getInstance().getTime().getTime + 2000, Some(Position(6.24927536, -75.5607389))),
+    PokemonPosition(3, "Magikarp", Calendar.getInstance().getTime().getTime + 2000, Some(Position(6.24618536, -75.5628389)))
+  )
+
+
+
+  def index = Action {
+    Ok(views.html.index(null))
+  }
+
+  def findPokemon = Action {
+    Ok( Json.toJson(pokemonList))
+  }
+
+  def findActivePokemon = Action.async(parse.json) { implicit request =>
+
+    request.body.validate[FindPokemon].map{
+      case find => {
+        val pokemonService = new PokemonServices
+        val respuesta = pokemonService.getCatchablePokemons(find)
+        Future(Ok(Json.toJson(respuesta )))
+      }
+    }.recoverTotal{
+      e => Future(BadRequest("Detected error:"+ JsError.toFlatJson(e)))
+    }
+  }
+
+  def findPokeStop = Action.async(parse.json) { implicit request =>
+
+    request.body.validate[FindPokemon].map{
+      case find => {
+        val pokemonService = new PokemonServices
+        val respuesta = pokemonService.getPokeStop(find)
+        Future(Ok(Json.toJson(respuesta )))
+      }
+    }.recoverTotal{
+      e => Future(BadRequest("Detected error:"+ JsError.toFlatJson(e)))
+    }
+  }
+
+  def findPokeGym = Action.async(parse.json) { implicit request =>
+
+    request.body.validate[FindPokemon].map{
+      case find => {
+        val pokemonService = new PokemonServices
+        val respuesta = pokemonService.getGyms(find)
+
+        Future(Ok(Json.toJson(respuesta )))
+      }
+    }.recoverTotal{
+      e => Future(BadRequest("Detected error:"+ JsError.toFlatJson(e)))
+    }
+  }
+
+  def db = Action {
+    var out = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(out)
+  }
+}
---
+++ app/pokemon/PokemonServices.scala
@@ -0,0 +1,318 @@
+package pokemon
+
+import java.io.{BufferedReader, IOException, InputStream, InputStreamReader}
+
+import POGOProtos.Map.Pokemon.MapPokemonOuterClass.MapPokemon
+import POGOProtos.Map.Pokemon.NearbyPokemonOuterClass.NearbyPokemon
+import POGOProtos.Map.Pokemon.WildPokemonOuterClass.WildPokemon
+import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo
+import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeTokenRequest, GoogleClientSecrets, GoogleTokenResponse}
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.pokegoapi.api.PokemonGo
+import com.pokegoapi.api.map.MapObjects
+import com.pokegoapi.api.map.pokemon.CatchablePokemon
+import com.pokegoapi.util.Log
+import dto._
+import okhttp3.OkHttpClient
+import com.pokegoapi.auth._
+
+import scala.collection.JavaConversions._
+
+/**
+  * Created by arcearta on 2016/07/26.
+  */
+
+
+class PokemonServices extends App{
+
+  override def main(args: Array[String]) {
+    println("Hello, world")
+  }
+
+  def convertStreamToString(is : InputStream) : String = {
+    def inner(reader : BufferedReader, sb : StringBuilder) : String = {
+      val line = reader.readLine()
+      if(line != null) {
+        try {
+          inner(reader, sb.append(line + "\n"))
+        } catch {
+          case e : IOException => e.printStackTrace()
+        } finally {
+          try {
+            is.close()
+          } catch {
+            case e : IOException => e.printStackTrace()
+          }
+        }
+
+      }
+      sb.toString()
+    }
+
+    inner(new BufferedReader(new InputStreamReader(is)), new StringBuilder())
+  }
+
+  @throws[IOException]
+  private def refreshMyToken(auth_code: String): String = {
+    System.out.println("auth_code on refreshMyToken: " + auth_code)
+
+    var refreshToken: String = "";
+    val saveCode = UserSession.findSession(auth_code)
+
+    println("saveCode: " + saveCode)
+
+    if(saveCode.isDefined){
+      System.out.println("refresh token encontrado: " + saveCode)
+      refreshToken = saveCode.get
+    }else{
+      val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance, new BufferedReader(new InputStreamReader(getUrl)))
+      println("paso1: " +clientSecrets.getDetails.getClientSecret )
+      val tokenResponse: GoogleTokenResponse = new GoogleAuthorizationCodeTokenRequest(new NetHttpTransport, JacksonFactory.getDefaultInstance, "https://www.googleapis.com/oauth2/v4/token", clientSecrets.getDetails.getClientId, clientSecrets.getDetails.getClientSecret, auth_code, "").execute
+      println("paso2: " + tokenResponse )
+      val accessToken: String = tokenResponse.getAccessToken
+      refreshToken= tokenResponse.getRefreshToken
+      System.out.println("New refresh token: " + refreshToken)
+      if(refreshToken != null && !"".equals(refreshToken) )
+        UserSession.saveSession(auth_code, refreshToken)
+    }
+    refreshToken
+  }
+
+  def getUrl: InputStream = {
+    val dato = this.getClass.getResourceAsStream("/" + "client_secret.json")
+    println("--------------------")
+    //println(convertStreamToString(dato))
+    println("--------------------")
+    dato
+  }
+
+  def authenticate(token : Option[String], http : OkHttpClient): (CredentialProvider, OkHttpClient) = {
+    var auth:CredentialProvider = null
+
+    /*if (token.isDefined && !"".equals(token.get)) {
+
+      println("Autenticando en google")
+      val refres = refreshMyToken(token.get)
+      println("Autenticando en google: refr : " + refres)
+      if (refres == null || "".equals(refres)) {
+        println("Autenticando con usuario y password")
+        auth = new GoogleAutoCredentialProvider(http, "iotpruebas@gmail.com", "Sura2016")
+      } else {
+        auth = new GoogleCredentialProvider(http, refres)
+      }
+      //auth = new GoogleCredentialProvider(http, findPokemon.token.get); // currently uses oauth flow so no user or pass needed
+    } else {
+      println("Autenticando en PTC")
+      auth = new PtcCredentialProvider(http, "pokeservices", "pokeservices")
+    } dsf*/
+    println("autenticando con itpruebas")
+    //auth = new GoogleAutoCredentialProvider(http, "iotpruebas@gmail.com", "Sura2016")
+    auth = new GoogleCredentialProvider(http, token.get)
+    (auth, http)
+  }
+
+  def getAllNearPokemons(findPokemon: FindPokemon): List[PokemonPosition] = {
+    val http: OkHttpClient = new OkHttpClient
+    var listPokemons = List[PokemonPosition]()
+    try {
+      val datos = authenticate(findPokemon.token, http)
+      val go: PokemonGo = new PokemonGo(datos._1, datos._2)
+
+      Thread.sleep(4000)
+
+      //6.254010, -75.578931
+      go.setLocation(findPokemon.position.get.latitud, findPokemon.position.get.longitud, 0)
+      val spawnPoints: MapObjects = go.getMap.getMapObjects(findPokemon.width)
+
+      val catchablePokemon: List[MapPokemon] = spawnPoints.getCatchablePokemons.toList
+      println("catchablePokemon in area:" + catchablePokemon.size)
+
+      catchablePokemon.foreach(cp => {
+        listPokemons = listPokemons ++ List(PokemonPosition(cp.getPokemonId.getNumber, cp.getPokemonId.name, cp.getExpirationTimestampMs, Some(Position(cp.getLatitude, cp.getLongitude))))
+      })
+      listPokemons
+
+     /* val nearbyPokemons: List[NearbyPokemon] = spawnPoints.getNearbyPokemons.toList
+      println("nearbyPokemons in area:" + nearbyPokemons.size)
+
+      nearbyPokemons.foreach(cp => {
+        listPokemons = listPokemons ++ List(PokemonPosition(cp.getPokemonId.getNumber, cp.getPokemonId.name, 0, Some(Position(cp.getLatitude, cp.getLongitude))))
+      })
+      listPokemons*/
+
+
+    }
+    catch {
+      case e: Any => {
+        Log.e("Main", "Failed to login or server issue: ", e)
+        listPokemons
+      }
+    }
+  }
+
+  def getCatchablePokemons(findPokemon: FindPokemon): List[PokemonPosition] = {
+    val http: OkHttpClient = new OkHttpClient
+
+    var listPokemons = List[PokemonPosition]()
+
+    try {
+
+      val datos = authenticate(findPokemon.token, http)
+      val go: PokemonGo = new PokemonGo(datos._1, datos._2)
+
+      Thread.sleep(4000)
+
+      //6.254010, -75.578931
+      go.setLocation(findPokemon.position.get.latitud, findPokemon.position.get.longitud, 0)
+      val spawnPoints: MapObjects = go.getMap.getMapObjects(findPokemon.width)
+
+      val catchablePokemon: List[MapPokemon] = spawnPoints.getCatchablePokemons.toList
+
+      println("Pokemon in area:" + catchablePokemon.size)
+
+      catchablePokemon.foreach(cp => {
+        listPokemons = listPokemons ++ List(PokemonPosition(cp.getPokemonId.getNumber, cp.getPokemonId.name, cp.getExpirationTimestampMs, Some(Position(cp.getLatitude, cp.getLongitude))))
+      })
+      listPokemons
+    }
+    catch {
+      case e: Any => {
+        Log.e("Main", "Failed to login or server issue: ", e)
+        listPokemons
+      }
+    }
+  }
+
+
+  def getNearPokemon(findPokemon: FindPokemon): List[PokemonPosition] = {
+    val http: OkHttpClient = new OkHttpClient
+
+    var listPokemons = List[PokemonPosition]()
+
+    try {
+
+      val datos = authenticate(findPokemon.token, http)
+      val go: PokemonGo = new PokemonGo(datos._1, datos._2)
+
+      Thread.sleep(4000)
+
+      //6.254010, -75.578931
+      go.setLocation(findPokemon.position.get.latitud, findPokemon.position.get.longitud, 0)
+      val spawnPoints: MapObjects = go.getMap.getMapObjects(findPokemon.width)
+
+      val catchablePokemon: List[WildPokemon] = spawnPoints.getWildPokemons.toList
+
+      println("Pokemon in area:" + catchablePokemon.size)
+
+      catchablePokemon.foreach(cp => {
+
+        listPokemons = listPokemons ++ List(PokemonPosition(cp.getPokemonData.getPokemonId.getNumber, cp.getPokemonData.getPokemonId.name, cp.getTimeTillHiddenMs, Some(Position(cp.getLatitude, cp.getLongitude))))
+      })
+      listPokemons
+    }
+    catch {
+      case e: Any => {
+        Log.e("Main", "Failed to login or server issue: ", e)
+        listPokemons
+      }
+    }
+  }
+
+
+  def getGyms(findPokemon: FindPokemon): List[Gym] = {
+    val http: OkHttpClient = new OkHttpClient
+
+    try {
+      val datos = authenticate(findPokemon.token, http)
+      val go: PokemonGo = new PokemonGo(datos._1, datos._2)
+
+      Thread.sleep(4000)
+
+      go.setLocation(findPokemon.position.get.latitud, findPokemon.position.get.longitud, 0)
+      val spawnPoints: MapObjects = go.getMap.getMapObjects(findPokemon.width)
+
+      println("Point in area:" + spawnPoints.isComplete)
+      println("Gyms :" + spawnPoints.getGyms.size())
+
+      val listGyms = spawnPoints.getGyms.toList.map(gym => Gym(gym.getOwnedByTeam.name, Position(gym.getLatitude, gym.getLongitude)))
+      listGyms
+    }
+    catch {
+      case e: Any => {
+        Log.e("Main", "Failed to login or server issue: ", e)
+        List[Gym]()
+      }
+    }
+  }
+
+  def getPokeStop(findPokemon: FindPokemon): List[Stop] = {
+    val http: OkHttpClient = new OkHttpClient
+
+    try {
+      val datos = authenticate(findPokemon.token, http)
+      val go: PokemonGo = new PokemonGo(datos._1, datos._2)
+      Thread.sleep(4000)
+
+      go.setLocation(findPokemon.position.get.latitud, findPokemon.position.get.longitud, 0)
+
+      val spawnPoints: MapObjects = go.getMap.getMapObjects(findPokemon.width)
+
+      println("Point in area:" + spawnPoints.isComplete)
+      println("PokeStops :" + spawnPoints.getPokestops.size())
+
+      val pokeStops = spawnPoints.getPokestops.map(stop => Stop("", Position(stop.getLatitude, stop.getLongitude)))
+      pokeStops.toList
+
+    }
+    catch {
+      case e: Any => {
+        Log.e("Main", "Failed to login or server issue: ", e)
+        List[Stop]()
+      }
+    }
+  }
+
+
+  def getCatchablePokemon {
+    val http: OkHttpClient = new OkHttpClient
+    try {
+      val token: String = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBiZDEwY2JmMDM2OGQ2MWE0NDBiZjYxZjNiM2EyZDI0NGExODQ5NDcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoib2ljcGdidS00Q1d1SFdLSEdNRDZ4dyIsImF1ZCI6Ijg0ODIzMjUxMTI0MC03M3JpM3Q3cGx2azk2cGo0Zjg1dWo4b3RkYXQyYWxlbS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExMTQyMTY1MjcxMjA1NzEwMDc1MCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI4NDgyMzI1MTEyNDAtNzNyaTN0N3Bsdms5NnBqNGY4NXVqOG90ZGF0MmFsZW0uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6ImFhcmlhc3RhQGdtYWlsLmNvbSIsImlhdCI6MTQ2OTU0Nzk5NiwiZXhwIjoxNDY5NTUxNTk2fQ.CEFnZW6nikCiGiF-_YtvgiZuK7GRHDlUlGCor0ZkCYKYb2ULntMj741JMWaWnG_RScpj_lycsFrAmGlxvy9qdv-0oOM5bmOIGjYPQVBSrYXncJ5lazAHlnIplUICHgv_bfE00C_yuaShCkLgBpXoaOgHdQp86WlBqLHb8CN3NBJk2CUUKZa6skTFGDOEgTgwSE1JEaanTTKr-3b6sfod-hwTbEIsMO5IoNNma4jp7E1LACl_3VBN1hOA4ZbTvOReSSVztkcIIdPTcM8styinPAg983u5nn_fApxHcvgK-m5-SUS9KWp9EsJkVQAstbP79Dg5SJrnq3ubm0r-4Z5z9g"
+      val auth = new GoogleCredentialProvider(http, token); // currently uses oauth flow so no user or pass needed
+      val go: PokemonGo = new PokemonGo(auth, http)
+      go.setLocation(6.254010, -75.578931, 0)
+
+      val catchablePokemon: List[CatchablePokemon] = go.getMap.getCatchablePokemon.toList
+      println("Pokemon in area:" + catchablePokemon.size)
+
+      /*catchablePokemon.foreach(cp => {
+        val encResult: EncounterResult = cp.encounterPokemon
+        if (encResult.wasSuccessful) {
+          println("Encounted:" + cp.getPokemonId)
+          val result: CatchResult = cp.catchPokemonWithRazzBerry
+          println("Attempt to catch:" + cp.getPokemonId + " " + result.getStatus)
+        }
+      })*/
+
+
+      val spawnPoints: MapObjects = go.getMap.getMapObjects(6.254010, -75.578931)
+
+      println("Point in area:" + spawnPoints.isComplete)
+
+      spawnPoints.getGyms.toList.foreach(cp => {
+        println("latitud:" + cp.getLatitude)
+        println("longitud:" + cp.getLongitude)
+        println("nombre:" + cp.getSponsor.name)
+        println("color:" + cp.getOwnedByTeam.name)
+      }
+      )
+
+    }
+    catch {
+      case e: Any => {
+        Log.e("Main", "Failed to login or server issue: ", e)
+      }
+    }
+  }
+
+}
